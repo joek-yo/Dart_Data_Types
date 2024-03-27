fd40295463d6116c86830074a8862ed65e5a5ee4
joek-yo <jkihiu129@gmail.com>
2024-03-27 15:25:12 +0300

    Data Types in Dart

---
+++ Dart_Types.dart
@@ -0,0 +1,21 @@
+void main() {
+    // Integer (int) - Stores whole numbers
+    int age = 30;
+    print("My age is: $age");
+
+    // Double (double) - stores decimal numbers
+    double pi = 3.14159;
+    print("The value of pi is: $pi");
+
+    // String (String) - Stores text data
+    String name = "Joseph Kihiu";
+    print("Hello, my name is: $name");
+
+    // List (List) - Stores an ordered collection of items
+    List<String> colors = ["red", "green", "blue"];
+    print("Primary colors: $colors");
+
+    // Map (Map) - Stores key-value pairs
+    Map<String, String> fruits = {"apple": "red", "banana": "yellow"};
+    print("Fruit color: ${fruits["apple"]}");
+}
